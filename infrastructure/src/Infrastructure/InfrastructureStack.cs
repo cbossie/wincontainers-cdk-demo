952eb12346fae4dfa9def3a877e31ee786c65d59
cbossie <cbossie@gmail.com>
2021-11-21 22:58:46 -0500

    Constructs

--- infrastructure/src/Infrastructure/InfrastructureStack.cs
+++ infrastructure/src/Infrastructure/InfrastructureStack.cs
@@ -1,13 +1,75 @@
+using System.Security.Cryptography;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.IO;
 using Amazon.CDK;
+using Amazon.CDK.RegionInfo;
 using Constructs;
+using Amazon.CDK.AWS.EC2;
+using Amazon.CDK.AWS.DirectoryService;
+using Amazon.CDK.AWS.SSM;
+using Amazon.CDK.AWS.SecretsManager;
+using Amazon.CDK.AWS.FSx;
+using static Amazon.CDK.AWS.DirectoryService.CfnMicrosoftAD;
 
 namespace Infrastructure
 {
     public class InfrastructureStack : Stack
     {
+
         internal InfrastructureStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
         {
-            // The code that defines your stack goes here
+
+            // Part 1 - Network
+            var vpc = new Vpc(this, "StackVpc", new VpcProps{
+                Cidr = "10.0.0.0/16",
+                MaxAzs = 3,
+                NatGateways = 2
+            });
+
+            // Part 2 - Domain Password
+            var gen = new Amazon.CDK.AWS.SecretsManager.SecretStringGenerator{
+                PasswordLength = 10
+            };
+            var secret = new Secret(this, "adpassword", new SecretProps{
+                Description = "Active Directory Password",
+                SecretName = "adpassword",
+                GenerateSecretString = gen
+            });
+
+
+            // Part 3 - Domain
+
+
+
+            var dir = new CfnMicrosoftAD(this, "StackAD", new CfnMicrosoftADProps{
+                Edition = "Standard",
+                Name = "wincontainer.local",
+                Password = secret.SecretValue.ToString(),
+                VpcSettings = new VpcSettingsProperty{
+                    VpcId = vpc.VpcId,
+                    SubnetIds = vpc.PrivateSubnets.Take(2).Select(a => a.SubnetId).ToArray()
+                },
+                ShortName = "wincontainer"
+            });
+
+            // Part 4 - FSX
+            var fsx = new Amazon.CDK.AWS.FSx.CfnFileSystem(this, "fileSystem", new CfnFileSystemProps{
+                FileSystemType = "WINDOWS",
+                StorageCapacity = 100,
+                StorageType = "SSD",
+                SubnetIds = vpc.PrivateSubnets.Take(2).Select(a => a.SubnetId).ToArray(),
+                WindowsConfiguration = new CfnFileSystem.WindowsConfigurationProperty {
+                    ActiveDirectoryId = Amazon.CDK.Fn.Ref(dir.LogicalId),
+                    DeploymentType = "MULTI_AZ_1",
+                    PreferredSubnetId = vpc.PrivateSubnets.First().SubnetId,
+                    ThroughputCapacity = 8,
+                },
+            });
+
+            // Part 5 ECS Cluster
+            
+
         }
     }
 }
--- infrastructure/src/Infrastructure/Program.cs
+++ infrastructure/src/Infrastructure/Program.cs
@@ -2,41 +2,32 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Infrastructure; 
 
-namespace Infrastructure
+var app = new App();
+new InfrastructureStack(app, "InfrastructureStack", new StackProps
 {
-    sealed class Program
+    // If you don't specify 'env', this stack will be environment-agnostic.
+    // Account/Region-dependent features and context lookups will not work,
+    // but a single synthesized template can be deployed anywhere.
+
+    // Uncomment the next block to specialize this stack for the AWS Account
+    // and Region that are implied by the current CLI configuration.
+    /*
+    Env = new Amazon.CDK.Environment
     {
-        public static void Main(string[] args)
-        {
-            var app = new App();
-            new InfrastructureStack(app, "InfrastructureStack", new StackProps
-            {
-                // If you don't specify 'env', this stack will be environment-agnostic.
-                // Account/Region-dependent features and context lookups will not work,
-                // but a single synthesized template can be deployed anywhere.
-
-                // Uncomment the next block to specialize this stack for the AWS Account
-                // and Region that are implied by the current CLI configuration.
-                /*
-                Env = new Amazon.CDK.Environment
-                {
-                    Account = System.Environment.GetEnvironmentVariable("CDK_DEFAULT_ACCOUNT"),
-                    Region = System.Environment.GetEnvironmentVariable("CDK_DEFAULT_REGION"),
-                }
-                */
-
-                // Uncomment the next block if you know exactly what Account and Region you
-                // want to deploy the stack to.
-                /**/
-                Env = new Amazon.CDK.Environment
-                {
-                    Account = "425173371283",
-                    Region = "us-east-2",
-                }
-                // For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-            });
-            app.Synth();
-        }
+        Account = System.Environment.GetEnvironmentVariable("CDK_DEFAULT_ACCOUNT"),
+        Region = System.Environment.GetEnvironmentVariable("CDK_DEFAULT_REGION"),
     }
-}
+    */
+
+    // Uncomment the next block if you know exactly what Account and Region you
+    // want to deploy the stack to.
+    /**/
+    Env = new Amazon.CDK.Environment
+    {
+        Account = "425173371283",
+        Region = "us-east-2",
+    }
+});
+app.Synth();
